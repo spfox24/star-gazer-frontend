803a935ae068f9dd5667ad790e37f8a767907b71
spfox24 <68411909+spfox24@users.noreply.github.com>
2021-09-20 12:10:24 -0500

    updates api

--- src/services/nasaService.js
+++ src/services/nasaService.js
@@ -1,5 +1,5 @@
 // const BASE_URL = 'http://localhost:3001/api/apod';
-const BASE_URL = 'https://nasa-star-gazer.herokuapp.com/https://star-gazer.netlify.app/';
+const BASE_URL = 'https://nasa-star-gazer.herokuapp.com/api';
 
 function fetchApodData() {
     return fetch(BASE_URL).then(res => res.json());
